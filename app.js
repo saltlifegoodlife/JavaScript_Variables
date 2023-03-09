226d592817df0ba663bb25cef45560ecbfbffe17
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-09 11:13:23 -0600

    created variables for exercises

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Ross";
+let lastName = "Beckham";
+var age = 33;
 
-// Exercise 2
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stack = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
